d4d2b9c40e530cb474f1e3f75ffbf6a9f4d8ce16
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2021-12-27 23:41:11 +0530

    details display functionality added

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ function filterOpen() {
     filterContainer.style.display = "flex";
   }
 }
+
+function closeSinglePersonSection() {
+  const section = document.getElementById("single-person");
+  const cardsSection = document.getElementById("cards-wrapper");
+
+  section.style.display = "none";
+  cardsSection.style.display = "block";
+}
